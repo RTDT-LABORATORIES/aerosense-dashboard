8e6db2993945c3d1a3f243060a531307e5e810f1
cortadocodes <cortado.codes@protonmail.com>
2022-07-05 12:42:01 +0100

    FIX: Separate dashboard.queries import from BQ client instantiation

--- dashboard/components/installation_select.py
+++ dashboard/components/installation_select.py
@@ -1,14 +1,16 @@
 from dash import dcc
 
-from ..queries import get_installations
+from ..queries import BigQuery
 
 
 def InstallationSelect():
-    installations = get_installations().to_dict(orient="records")
+    installations = BigQuery().get_installations().to_dict(orient="records")
+
     options = [
         {"label": f"{row['reference']} (Turbine {row['turbine_id']})", "value": row["reference"]}
         for row in installations
     ]
+
     return dcc.Dropdown(
         options=options,
         id="installation_select",
--- dashboard/queries.py
+++ dashboard/queries.py
@@ -3,9 +3,6 @@ import datetime
 from google.cloud import bigquery
 
 
-bqclient = bigquery.Client()
-
-
 # SCRATCH: MIGHT BE USEFUL, OTHERWISE GET RID:
 # ============================================
 # AND datetime > DATE_ADD(PARSE_DATETIME('%Y%m%d', @DS_START_DATE), INTERVAL @hour HOUR)
@@ -16,61 +13,64 @@ bqclient = bigquery.Client()
 # AND IS_NAN(sensor_value[ORDINAL(4)]) IS FALSE
 
 
-def get_aggregated_connection_statistics(installation_reference, start=None, finish=None, all_time=False):
-    """Query for minute-wise aggregated connection statistics over a day, by default the day up to now.
+class BigQuery:
+    def __init__(self):
+        self.client = bigquery.Client()
 
-    :param [str] installation_reference: The installation reference to query for, e.g. "ost-wt-tests"
-    :param Union[datetime.datetime, None] up_to: The point in time, default now(), where results will
-    be delivered for the 24 hours prior.
-    """
-    connection_statistics_agg_sql = """
-    SELECT datetime, filtered_rssi, raw_rssi, tx_power, allocated_heap_memory
-    FROM `aerosense-twined.greta.connection_statistics_agg`
-    WHERE datetime BETWEEN @start AND @finish
-    AND installation_reference = @installation_reference
-    """
+    def get_aggregated_connection_statistics(self, installation_reference, start=None, finish=None, all_time=False):
+        """Query for minute-wise aggregated connection statistics over a day, by default the day up to now.
 
-    if all_time:
-        start = datetime.datetime.min
-        finish = datetime.datetime.now()
-    else:
-        finish = finish if finish else datetime.datetime.now()
-        start = start if start else finish - datetime.timedelta(days=1)
+        :param [str] installation_reference: The installation reference to query for, e.g. "ost-wt-tests"
+        :param Union[datetime.datetime, None] up_to: The point in time, default now(), where results will
+        be delivered for the 24 hours prior.
+        """
+        connection_statistics_agg_sql = """
+        SELECT datetime, filtered_rssi, raw_rssi, tx_power, allocated_heap_memory
+        FROM `aerosense-twined.greta.connection_statistics_agg`
+        WHERE datetime BETWEEN @start AND @finish
+        AND installation_reference = @installation_reference
+        """
 
-    query_config = bigquery.QueryJobConfig(
-        query_parameters=[
-            bigquery.ScalarQueryParameter("installation_reference", "STRING", installation_reference),
-            bigquery.ScalarQueryParameter("start", "DATETIME", start),
-            bigquery.ScalarQueryParameter("finish", "DATETIME", finish),
-        ]
-    )
+        if all_time:
+            start = datetime.datetime.min
+            finish = datetime.datetime.now()
+        else:
+            finish = finish if finish else datetime.datetime.now()
+            start = start if start else finish - datetime.timedelta(days=1)
 
-    return bqclient.query(connection_statistics_agg_sql, job_config=query_config).to_dataframe()
+        query_config = bigquery.QueryJobConfig(
+            query_parameters=[
+                bigquery.ScalarQueryParameter("installation_reference", "STRING", installation_reference),
+                bigquery.ScalarQueryParameter("start", "DATETIME", start),
+                bigquery.ScalarQueryParameter("finish", "DATETIME", finish),
+            ]
+        )
 
+        return self.client.query(connection_statistics_agg_sql, job_config=query_config).to_dataframe()
 
-def get_installations():
-    """Query for all installations (without sensor coordinate data)"""
-    installations_sql = """
-    SELECT reference, turbine_id, location
-    FROM `aerosense-twined.greta.installation`
-    """
+    def get_installations(self):
+        """Query for all installations (without sensor coordinate data)"""
+        installations_sql = """
+        SELECT reference, turbine_id, location
+        FROM `aerosense-twined.greta.installation`
+        """
 
-    return bqclient.query(installations_sql).to_dataframe()
+        return self.client.query(installations_sql).to_dataframe()
 
+    def get_installation(self, installation_reference):
+        """Query for an installation (with sensor coordinate data)
+        May return multiple rows if the installation has had upgraded hardware
 
-def get_installation(installation_reference):
-    """Query for an installation (with sensor coordinate data)
-    May return multiple rows if the installation has had upgraded hardware
+        :param [str] installation_reference: The installation reference to query for, e.g. "ost-wt-tests"
+        """
+        installation_sql = """
+        SELECT reference, turbine_id, location
+        FROM `aerosense-twined.greta.installation`
+        WHERE installation_reference = @installation_reference
+        """
 
-    :param [str] installation_reference: The installation reference to query for, e.g. "ost-wt-tests"
-    """
-    installation_sql = """
-    SELECT reference, turbine_id, location
-    FROM `aerosense-twined.greta.installation`
-    WHERE installation_reference = @installation_reference
-    """
+        query_config = bigquery.QueryJobConfig(
+            query_parameters=[bigquery.ScalarQueryParameter("installation_reference", "STRING", installation_reference)]
+        )
 
-    query_config = bigquery.QueryJobConfig(
-        query_parameters=[bigquery.ScalarQueryParameter("installation_reference", "STRING", installation_reference)]
-    )
-    return bqclient.query(installation_sql, job_config=query_config).to_dataframe()
+        return self.client.query(installation_sql, job_config=query_config).to_dataframe()
