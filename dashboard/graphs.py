e567d14d48744cf5dedc97344168e5fc84335ffe
cortadocodes <cortado.codes@protonmail.com>
2022-07-11 12:19:53 +0100

    FEA: Add ability to plot sensor data

--- dashboard/graphs.py
+++ dashboard/graphs.py
@@ -5,7 +5,19 @@ from plotly import express as px
 from dashboard.queries import BigQuery
 
 
-def plot_connections_statistics(installation_reference, y_axis_column, time_range):
+def _generate_time_range(time_range):
+    """Generate a convenient time range to plot. The options are:
+    - Last minute
+    - Last hour
+    - Last day
+    - Last week
+    - Last month
+    - Last year
+    - All time
+
+    :param str time_range:
+    :return (datetime.datetime, datetime.datetime, bool): the start datetime, finish datetime, and whether all time has been selected
+    """
     time_range_options = {
         "Last minute": datetime.timedelta(minutes=1),
         "Last hour": datetime.timedelta(hours=1),
@@ -24,6 +36,12 @@ def plot_connections_statistics(installation_reference, y_axis_column, time_rang
         finish = datetime.datetime.now()
         start = finish - time_range_options[time_range]
 
+    return start, finish, all_time
+
+
+def plot_connections_statistics(installation_reference, y_axis_column, time_range):
+    start, finish, all_time = _generate_time_range(time_range)
+
     df = BigQuery().get_aggregated_connection_statistics(
         installation_reference,
         start=start,
@@ -32,4 +50,20 @@ def plot_connections_statistics(installation_reference, y_axis_column, time_rang
     )
 
     figure = px.line(df, x="datetime", y=y_axis_column)
+    return figure
+
+
+def plot_sensors(installation_reference, node_id, sensor_name, time_range):
+    start, finish, all_time = _generate_time_range(time_range)
+
+    df = BigQuery().get_sensor_data(
+        installation_reference,
+        node_id,
+        sensor_name,
+        start=start,
+        finish=finish,
+        all_time=all_time,
+    )
+
+    figure = px.line(df, x="datetime", y=sensor_name)
     return figure
