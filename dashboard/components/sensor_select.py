b1244cfb47f6776857107ddd5b372289ecd39b25
cortadocodes <cortado.codes@protonmail.com>
2023-03-02 16:22:37 +0000

    ENH: Allow `SensorSelect` to deal with new data format

--- dashboard/components/sensor_select.py
+++ dashboard/components/sensor_select.py
@@ -3,16 +3,16 @@ from dash import dcc
 from aerosense_tools.queries import BigQuery
 
 
+EXCLUDED_SENSORS = {"microphone", "information_sensors", "battery_info"}
+
+
 def SensorSelect():
-    sensor_types = [
-        sensor_type
-        for sensor_type in BigQuery().get_sensor_types()
-        if sensor_type not in {"microphone", "information_sensors", "battery_info"}
-    ]
+    sensor_types = [sensor for sensor in BigQuery().get_sensor_types() if sensor["name"] not in EXCLUDED_SENSORS]
+    sensor_names = [sensor["name"] for sensor in sensor_types]
 
     return dcc.Dropdown(
-        options=sensor_types,
+        options=sensor_names,
         id="y-axis-select",
-        value=sensor_types[0],
+        value=sensor_names[0],
         persistence=True,
     )
