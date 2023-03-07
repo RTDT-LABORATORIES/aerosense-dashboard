c23f279682b2743d4080630e0504112a908f7a30
cortadocodes <cortado.codes@protonmail.com>
2023-03-07 16:32:42 +0000

    REF: Move pressure profile layout into `layouts` module

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,13 +1,10 @@
 import dash
-import dash_daq as daq
-from dash import dcc, html
+from dash import html
 from flask_caching import Cache
 
 from aerosense_tools.queries import BigQuery
 from dashboard.callbacks import register_callbacks
-from dashboard.components import About, InstallationSelect, Logo, Navigation, Title
-from dashboard.components.node_select import NodeSelect
-from dashboard.layouts import create_sensors_tab_layout
+from dashboard.layouts import create_pressure_profile_tab_layout, create_sensors_tab_layout
 
 
 SENSOR_TYPES = BigQuery().get_sensor_types()
@@ -41,69 +38,7 @@ tabs = {
         graph_id="sensors-graph",
         data_limit_warning_id="sensor-data-limit-warning",
     ),
-    "pressure_profile": [
-        html.Div(
-            [
-                html.Div([Logo(app.get_asset_url("logo.png")), Title(), About()]),
-                Navigation(selected_tab="pressure_profile"),
-                html.Br(),
-                html.Div(
-                    [
-                        html.Label(html.B("Installation")),
-                        html.Label("Installation reference"),
-                        InstallationSelect(),
-                        html.Label("Node id"),
-                        NodeSelect(),
-                        html.Br(),
-                        html.Label(html.B("Start date/time")),
-                        html.Label("Date"),
-                        dcc.DatePickerSingle(
-                            id="date-select",
-                            display_format="Do MMM Y",
-                            persistence=True,
-                        ),
-                        html.Br(),
-                        html.Label("Hour"),
-                        daq.NumericInput(id="hour", value=0, min=0, max=23, persistence=True),
-                        html.Label("Minute"),
-                        daq.NumericInput(id="minute", value=0, min=0, max=59, persistence=True),
-                        html.Label("Second"),
-                        daq.NumericInput(id="second", value=0, min=0, max=59, persistence=True),
-                        html.Br(),
-                        html.Label(html.B("Step forward (seconds)")),
-                        html.Br(),
-                        dcc.Slider(
-                            id="time-slider",
-                            min=0,
-                            max=60,
-                            step=1,
-                            marks={i: str(i) for i in range(0, 60, 5)},
-                            value=0,
-                            updatemode="drag",
-                        ),
-                        html.Br(),
-                        html.Button("Plot", id="refresh-button", n_clicks=0),
-                        html.Button("Check for new installations", id="installation-check-button", n_clicks=0),
-                    ],
-                    id="buttons-section",
-                    className="sidebar-content",
-                ),
-            ],
-            className="four columns sidebar",
-        ),
-        html.Div(
-            [
-                html.Div(
-                    [
-                        html.H3("Pressure profile", id="graph-title"),
-                    ],
-                    className="text-box",
-                ),
-                dcc.Graph(id="pressure-profile-graph", style={"margin": "0px 20px", "height": "45vh"}),
-            ],
-            className="eight columns",
-        ),
-    ],
+    "pressure_profile": create_pressure_profile_tab_layout(app),
 }
 
 app.layout = html.Div(
--- dashboard/layouts.py
+++ dashboard/layouts.py
@@ -1,3 +1,4 @@
+import dash_daq
 from dash import dcc, html
 
 from dashboard.components import About, InstallationSelect, Logo, Navigation, Title
@@ -63,6 +64,77 @@ def create_sensors_tab_layout(app, tab_name, sensor_names, graph_id, data_limit_
                     className="text-box",
                 ),
                 dcc.Graph(id=graph_id, style={"margin": "0px 20px", "height": "45vh"}),
+            ],
+            className="eight columns",
+        ),
+    ]
+
+
+def create_pressure_profile_tab_layout(app):
+    """Create the layout corresponding to the pressure profile tab.
+
+    :param dash.Dash app:
+    :return list:
+    """
+    return [
+        html.Div(
+            [
+                html.Div([Logo(app.get_asset_url("logo.png")), Title(), About()]),
+                Navigation(selected_tab="pressure_profile"),
+                html.Br(),
+                html.Div(
+                    [
+                        html.Label(html.B("Installation")),
+                        html.Label("Installation reference"),
+                        InstallationSelect(),
+                        html.Label("Node id"),
+                        NodeSelect(),
+                        html.Br(),
+                        html.Label(html.B("Start date/time")),
+                        html.Label("Date"),
+                        dcc.DatePickerSingle(
+                            id="date-select",
+                            display_format="Do MMM Y",
+                            persistence=True,
+                        ),
+                        html.Br(),
+                        html.Label("Hour"),
+                        dash_daq.NumericInput(id="hour", value=0, min=0, max=23, persistence=True),
+                        html.Label("Minute"),
+                        dash_daq.NumericInput(id="minute", value=0, min=0, max=59, persistence=True),
+                        html.Label("Second"),
+                        dash_daq.NumericInput(id="second", value=0, min=0, max=59, persistence=True),
+                        html.Br(),
+                        html.Label(html.B("Step forward (seconds)")),
+                        html.Br(),
+                        dcc.Slider(
+                            id="time-slider",
+                            min=0,
+                            max=60,
+                            step=1,
+                            marks={i: str(i) for i in range(0, 60, 5)},
+                            value=0,
+                            updatemode="drag",
+                        ),
+                        html.Br(),
+                        html.Button("Plot", id="refresh-button", n_clicks=0),
+                        html.Button("Check for new installations", id="installation-check-button", n_clicks=0),
+                    ],
+                    id="buttons-section",
+                    className="sidebar-content",
+                ),
+            ],
+            className="four columns sidebar",
+        ),
+        html.Div(
+            [
+                html.Div(
+                    [
+                        html.H3("Pressure profile", id="graph-title"),
+                    ],
+                    className="text-box",
+                ),
+                dcc.Graph(id="pressure-profile-graph", style={"margin": "0px 20px", "height": "45vh"}),
             ],
             className="eight columns",
         ),
